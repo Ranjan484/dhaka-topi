fb02fc5adc6d48cd4f46a68edb80a42b30c49a51
Ranjan <ranjanrajthala24@gmai.com>
2021-05-25 12:10:17 +0545

    added Loader and Message component

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      //   useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    const conn = await mongoose.connect(
+      "mongodb+srv://dhakatopi:dhakatopi@cluster0.ynu4u.mongodb.net/dhakatopi?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        //   useFindAndModify: false,
+        useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB connected ${conn.connection.host}`.cyan.underline);
   } catch (err) {
     console.error(`Error: ${err.message}`.red.underline);
